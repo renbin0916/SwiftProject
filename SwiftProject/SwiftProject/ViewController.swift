23936a9a3a584595a81bd80e5b1261ed28f034ad
易 <yi@leegame.net>
2019-10-17 14:34:13 +0800

    Update - init

---
+++ SwiftProject/SwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftProject
+//
+//  Created by 易 on 17/10/2019.
+//  Copyright © 2019 易. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
